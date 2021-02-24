df2519ca95c59ccbb4a8821d6c0674a8b70bbe13
Martin Desrumaux <martin@letik.io>
2021-02-24 21:11:18 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# parcel-v2-vue-sfc
---
+++ src/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <p class="hey">Hey there {{ username }}!</p>
+</template>
+
+<script>
+import { defineComponent } from 'vue'
+
+export default defineComponent({
+  setup () {
+    return {
+      username: 'gnuletik'
+    }
+  }
+})
+</script>
+
+<style>
+.hey {
+  color: white;
+  background: black;
+}
+</style>
